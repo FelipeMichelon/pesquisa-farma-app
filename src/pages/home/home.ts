5640e17bf467a5bc068df4c778b88e67319689e8
Felipe Michelon <eng.michelon@gmail.com>
2017-03-31 15:11:03 -0300

    Logout também funcionando

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,18 +2,23 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LoginUsuarioPage } from '../login-usuario/login-usuario';
 
+import { AngularFire } from 'angularfire2';
+import firebase from 'firebase';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public angFire: AngularFire) {
 
-    window.localStorage.removeItem('currentuser');
+    //window.localStorage.removeItem('currentuser');
     if(!this.estaLogado()){
       console.log("Você não está logado.");
       this.navCtrl.push(LoginUsuarioPage);
+      //this.angFire.auth.logout();
     }
 
   }//constructor
@@ -23,6 +28,13 @@ export class HomePage {
       return true;
     }
   }//estaLogado
+
+  sair(){
+    console.log("Logout");
+    window.localStorage.removeItem('currentuser');
+    this.angFire.auth.logout();
+    this.navCtrl.push(LoginUsuarioPage);
+  }//sair
 
   btnHome(){
     console.log("Clicou o botão logo de home.");
