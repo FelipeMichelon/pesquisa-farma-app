47981369e1201ecd7ce8e49b13ba094988fd9982
Felipe Michelon <eng.michelon@gmail.com>
2017-03-31 13:36:55 -0300

    Login funcionando corretamente

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { LoginUsuarioPage } from '../login-usuario/login-usuario';
 
 @Component({
   selector: 'page-home',
@@ -10,7 +10,19 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
-  }
+    window.localStorage.removeItem('currentuser');
+    if(!this.estaLogado()){
+      console.log("Você não está logado.");
+      this.navCtrl.push(LoginUsuarioPage);
+    }
+
+  }//constructor
+
+  estaLogado(){
+    if(window.localStorage.getItem('currentuser')){
+      return true;
+    }
+  }//estaLogado
 
   btnHome(){
     console.log("Clicou o botão logo de home.");
